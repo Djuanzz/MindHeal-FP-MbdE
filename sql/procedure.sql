ab4f4677a54d04af60b40b92afab8e3e97840d1b
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2024-05-30 04:43:59 +0700

    fix: getScheduleForCurrentWeek procedure for distinct element

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -22,20 +22,35 @@ END
 CALL GetSchedulesByDate ('2024-05-07');
 CALL GetSchedulesByDate ('2024-05-06');
 
+-- CREATE PROCEDURE GetScheduleForCurrentWeek()
+-- BEGIN
+--     SELECT
+--       s.ScheduleDate,
+--       DAYNAME(s.ScheduleDate) AS DayName
+--     FROM
+--       schedule s
+--     WHERE
+--       s.ScheduleDate BETWEEN
+--         DATE_SUB(CURDATE(), INTERVAL (DAYOFWEEK(CURDATE()) - 2) DAY)
+--         AND DATE_ADD(DATE_SUB(CURDATE(), INTERVAL (DAYOFWEEK(CURDATE()) - 2) DAY), INTERVAL 5 DAY)
+--     ORDER BY
+--       s.ScheduleDate;
+-- END 
+
 CREATE PROCEDURE GetScheduleForCurrentWeek()
 BEGIN
-    SELECT
-      s.ScheduleDate,
-      DAYNAME(s.ScheduleDate) AS DayName
+    SELECT DISTINCT
+        s.ScheduleDate,
+        DAYNAME(s.ScheduleDate) AS DayName
     FROM
-      schedule s
+        schedule s
     WHERE
-      s.ScheduleDate BETWEEN
-        DATE_SUB(CURDATE(), INTERVAL (DAYOFWEEK(CURDATE()) - 2) DAY)
-        AND DATE_ADD(DATE_SUB(CURDATE(), INTERVAL (DAYOFWEEK(CURDATE()) - 2) DAY), INTERVAL 5 DAY)
+        s.ScheduleDate BETWEEN
+            DATE_SUB(CURDATE(), INTERVAL (DAYOFWEEK(CURDATE()) - 2) DAY)
+            AND DATE_ADD(DATE_SUB(CURDATE(), INTERVAL (DAYOFWEEK(CURDATE()) - 2) DAY), INTERVAL 5 DAY)
     ORDER BY
-      s.ScheduleDate;
-END 
+        s.ScheduleDate;
+END;
 
 CALL GetScheduleForCurrentWeek();
 

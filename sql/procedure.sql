ca376dfe0c67b9360a6dac444ade77b73861a3dc
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2024-05-30 03:33:10 +0700

    feat: get schedule for current week procedure

--- sql/procedure.sql
+++ sql/procedure.sql
@@ -21,3 +21,20 @@ END
 
 CALL GetSchedulesByDate ('2024-05-07');
 CALL GetSchedulesByDate ('2024-05-06');
+
+CREATE PROCEDURE GetScheduleForCurrentWeek()
+BEGIN
+    SELECT
+      s.ScheduleDate,
+      DAYNAME(s.ScheduleDate) AS DayName
+    FROM
+      schedule s
+    WHERE
+      s.ScheduleDate BETWEEN
+        DATE_SUB(CURDATE(), INTERVAL (DAYOFWEEK(CURDATE()) - 2) DAY)
+        AND DATE_ADD(DATE_SUB(CURDATE(), INTERVAL (DAYOFWEEK(CURDATE()) - 2) DAY), INTERVAL 5 DAY)
+    ORDER BY
+      s.ScheduleDate;
+END 
+
+CALL GetScheduleForCurrentWeek();
